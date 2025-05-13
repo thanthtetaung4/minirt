005639cad7b1fd075ba9624447a3310f6e6b18a9
Thant Htet Aung <taung@student.42singapore.sg>
2025-05-13 15:28:43 +0800

    data types added

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,7 +6,7 @@
 /*   By: taung <taung@student.42singapore.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 02:40:16 by taung             #+#    #+#             */
-/*   Updated: 2025/05/13 13:09:35 by taung            ###   ########.fr       */
+/*   Updated: 2025/05/13 15:27:49 by taung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,63 @@
 # include <X11/keysym.h>
 # include <X11/X.h>
 
+typedef struct s_xyz
+{
+	float	x;
+	float	y;
+	float	z;
+}	t_xyz;
+
+typedef struct s_rgb
+{
+	int	r;
+	int	g;
+	int	b;
+}	t_rgb;
+
+typedef struct s_ambient
+{
+	float	ratio;
+	int	color;
+}	t_ambient;
+
+typedef struct s_camera
+{
+	t_xyz	origin;
+	t_xyz	orientation;
+	float	fov;
+}	t_camera;
+
+typedef struct s_light
+{
+	t_xyz	origin;
+	float	ratio;
+	t_rgb	color;
+}	t_light;
+
+typedef struct s_sphere
+{
+	t_xyz	origin;
+	float	radius;
+	t_rgb	color;
+}	t_sphere;
+
+typedef struct s_plane
+{
+	t_xyz	origin;
+	t_xyz	orientation;
+	int	color;
+}	t_plane;
+
+typedef struct s_cylinder
+{
+	t_xyz	origin;
+	t_xyz	orientation;
+	float	radius;
+	float	height;
+	int	color;
+}	t_cylinder;
+
 typedef struct s_data
 {
 	int	i;
@@ -30,6 +87,15 @@ typedef struct s_data
 	int	win_width;
 	int	win_height;
 	char	**scene;
+	t_ambient	ambient;
+	t_camera	camera;
+	t_light	light;
+	t_sphere	sphere;
+	t_plane	plane;
+	t_cylinder	cylinder;
+	int	ambient_count;
+	int	camera_count;
+	int	light_count;
 }	t_data;
 
 int	handle_keypress(int keysym, t_data *data);
