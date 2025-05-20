58cc2ee202cd767f713e7ad2ffbc3e1a7f879950
novailable <97833342+novailable@users.noreply.github.com>
2025-05-21 03:52:50 +0800

    updated main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aoo <aoo@student.42singapore.sg>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 02:39:59 by taung             #+#    #+#             */
-/*   Updated: 2025/05/21 03:44:21 by aoo              ###   ########.fr       */
+/*   Updated: 2025/05/21 03:52:27 by aoo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,8 @@ int	main(int argc, char **argv)
 	print_data(data);
 	data.mlx = mlx_init();
 	data.mlx_win = mlx_new_window(data.mlx, 800, 600, "Hello World");
-	mlx_hook(data.mlx_win, DestroyNotify, NoEventMask, close_win, &data);
+	// DestoryNotify 17, NoEventMask 0
+	mlx_hook(data.mlx_win, 17, 0, close_win, &data);
 	mlx_loop(data.mlx);
 	if (parse_ambient(data.scene[1], &data.ambient) == 0)
 	{
